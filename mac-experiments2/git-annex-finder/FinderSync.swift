2c88747af3cec14eac10e9a3e729516b697ab0bc
Andrew Ringler <ringler@gmail.com>
2016-11-23 15:54:08 -0500

    migrate finder sync extension to latest Swift

--- mac-experiments2/git-annex-finder/FinderSync.swift
+++ mac-experiments2/git-annex-finder/FinderSync.swift
@@ -11,42 +11,42 @@ import FinderSync
 
 class FinderSync: FIFinderSync {
 
-    var myFolderURL: NSURL = NSURL(fileURLWithPath: "/Users/Shared/MySyncExtension Documents")
+    var myFolderURL: URL = URL(fileURLWithPath: "/Users/Shared/MySyncExtension Documents")
 
     override init() {
         super.init()
 
-        NSLog("FinderSync() launched from %@", NSBundle.mainBundle().bundlePath)
+        NSLog("FinderSync() launched from %@", Bundle.main.bundlePath)
 
         // Set up the directory we are syncing.
-        FIFinderSyncController.defaultController().directoryURLs = [self.myFolderURL]
+        FIFinderSyncController.default().directoryURLs = [self.myFolderURL]
         
         // Set up images for our badge identifiers. For demonstration purposes, this uses off-the-shelf images.
-        FIFinderSyncController.defaultController().setBadgeImage(NSImage(named: NSImageNameColorPanel)!, label: "Status One" , forBadgeIdentifier: "One")
-        FIFinderSyncController.defaultController().setBadgeImage(NSImage(named: NSImageNameCaution)!, label: "Status Two", forBadgeIdentifier: "Two")
+        FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImageNameColorPanel)!, label: "Status One" , forBadgeIdentifier: "One")
+        FIFinderSyncController.default().setBadgeImage(NSImage(named: NSImageNameCaution)!, label: "Status Two", forBadgeIdentifier: "Two")
     }
 
     // MARK: - Primary Finder Sync protocol methods
 
-    override func beginObservingDirectoryAtURL(url: NSURL) {
+    override func beginObservingDirectory(at url: URL) {
         // The user is now seeing the container's contents.
         // If they see it in more than one view at a time, we're only told once.
-        NSLog("beginObservingDirectoryAtURL: %@", url.filePathURL!)
+        debugPrint("beginObservingDirectoryAtURL: %@", (url as NSURL).filePathURL!)
     }
 
 
-    override func endObservingDirectoryAtURL(url: NSURL) {
+    override func endObservingDirectory(at url: URL) {
         // The user is no longer seeing the container's contents.
-        NSLog("endObservingDirectoryAtURL: %@", url.filePathURL!)
+        debugPrint("endObservingDirectoryAtURL: %@", (url as NSURL).filePathURL!)
     }
 
-    override func requestBadgeIdentifierForURL(url: NSURL) {
-        NSLog("requestBadgeIdentifierForURL: %@", url.filePathURL!)
+    override func requestBadgeIdentifier(for url: URL) {
+        debugPrint("requestBadgeIdentifierForURL: %@", (url as NSURL).filePathURL!)
         
         // For demonstration purposes, this picks one of our two badges, or no badge at all, based on the filename.
-        let whichBadge = abs(url.filePathURL!.hash) % 3
+        let whichBadge = abs(((url as NSURL).filePathURL! as NSURL).hash) % 3
         let badgeIdentifier = ["", "One", "Two"][whichBadge]
-        FIFinderSyncController.defaultController().setBadgeIdentifier(badgeIdentifier, forURL: url)
+        FIFinderSyncController.default().setBadgeIdentifier(badgeIdentifier, for: url)
     }
 
     // MARK: - Menu and toolbar item support
@@ -63,21 +63,21 @@ class FinderSync: FIFinderSync {
         return NSImage(named: NSImageNameCaution)!
     }
 
-    override func menuForMenuKind(menuKind: FIMenuKind) -> NSMenu {
+    override func menu(for menuKind: FIMenuKind) -> NSMenu {
         // Produce a menu for the extension.
         let menu = NSMenu(title: "")
-        menu.addItemWithTitle("git-annex", action: #selector(sampleAction(_:)), keyEquivalent: "")
+        menu.addItem(withTitle: "git-annex", action: #selector(sampleAction(_:)), keyEquivalent: "")
         return menu
     }
 
-    @IBAction func sampleAction(sender: AnyObject?) {
-        let target = FIFinderSyncController.defaultController().targetedURL()
-        let items = FIFinderSyncController.defaultController().selectedItemURLs()
+    @IBAction func sampleAction(_ sender: AnyObject?) {
+        let target = FIFinderSyncController.default().targetedURL()
+        let items = FIFinderSyncController.default().selectedItemURLs()
 
         let item = sender as! NSMenuItem
-        NSLog("sampleAction: menu item: %@, target = %@, items = ", item.title, target!.filePathURL!)
+        debugPrint("sampleAction: menu item: %@, target = %@, items = ", item.title, (target! as NSURL).filePathURL!)
         for obj in items! {
-            NSLog("    %@", obj.filePathURL!)
+            debugPrint("    %@", (obj as NSURL).filePathURL!)
         }
     }
 
