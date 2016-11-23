0706675af03d9bd50d6dc330ecb852cbcdc1e40a
Andrew Ringler <ringler@gmail.com>
2016-11-23 12:56:59 -0500

    create config file on launch of command line

--- mac-experiments2/git-annex-mac-cmd/main.swift
+++ mac-experiments2/git-annex-mac-cmd/main.swift
@@ -10,10 +10,20 @@ import Foundation
 
 //print("Hello, World!")
 
+var error: NSError?
+
+// Create configuration file
+// at ~/.config/git-annex/turtle-watch
+// to store list of git-annex directories to watch
+let dataPath = "\(NSHomeDirectory())/.config/git-annex/turtle-watch"
+if (!FileManager.default.fileExists(atPath: dataPath)) {
+    var success = FileManager.default.createFile(atPath: dataPath, contents: Data.init())
+}
+
 let finder = Finder()
 if CommandLine.argc < 2 {
     finder.interactiveMode()
 } else {
-    finder.staticMode() 
+    finder.staticMode()
 }
 
