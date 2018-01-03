fec5eb63c4d32b9ea4b338c95f306ca74a319271
Andrew Ringler <andrew@andrewringler.com>
2018-01-03 14:21:41 -0500

    menubar icon for turtule, quit finder sync on quit

--- mac-experiments2/git-annex-mac/AppDelegate.swift
+++ mac-experiments2/git-annex-mac/AppDelegate.swift
@@ -6,69 +6,103 @@
 //  Copyright © 2016 Andrew Ringler. All rights reserved.
 //
 import Cocoa
-import Foundation
+//import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name(rawValue: "git-annex-menubar-default"))
+            button.action = #selector(printQuote(_:))
+        }
+        constructMenu()
 
-        let defaults = UserDefaults(suiteName: "group.com.andrewringler.git-annex-mac.sharedgroup")
-        let config = Config()
-        
-        // just grab the first repo to watch, for now
-        let repo :String? = config.listWatchedRepos().first
-        
-        if defaults != nil && repo != nil {
-            var myFolderURL = URL(fileURLWithPath: repo!)
+        DispatchQueue.global(qos: .background).async {
+            let defaults = UserDefaults(suiteName: "group.com.andrewringler.git-annex-mac.sharedgroup")
+            let config = Config()
             
-            // FinderSync will read this
-            defaults!.synchronize()
-            defaults!.set(repo, forKey: "myFolderURL")
-            defaults!.synchronize()
+            // just grab the first repo to watch, for now
+            let repo :String? = config.listWatchedRepos().first
             
-            // see https://github.com/kpmoran/OpenTerm/commit/022dcfaf425645f63d4721b1353c31614943bc32
-            let task = Process()
-            task.launchPath = "/bin/bash"
-            task.arguments = ["-c", "pluginkit -e use -i com.andrewringler.git-annex-mac.git-annex-finder ; killall Finder"]
-            task.launch()
-            
-            while true {
-                let allKeys = defaults!.dictionaryRepresentation().keys
-                for key in allKeys {
-                    if key.starts(with: "gitannex.") {
-                        if(defaults!.string(forKey: key)! == "request"){
-                            var url :String = key
-                            url.removeFirst("gitannex.".count)
-                            let status = GitAnnexQueries.gitAnnexPathInfo(for: URL(fileURLWithPath: url), in: (myFolderURL as NSURL).path!)
-                            
-                            if status == "present" {
-                                defaults!.set("present", forKey: key)
-                            } else if status == "absent" {
-                                defaults!.set("absent", forKey: key)
-                            } else {
-                                defaults!.set("unknown", forKey: key)
+            if defaults != nil && repo != nil {
+                let myFolderURL = URL(fileURLWithPath: repo!)
+                
+                // FinderSync will read this
+                defaults!.synchronize()
+                defaults!.set(repo, forKey: "myFolderURL")
+                defaults!.synchronize()
+                
+                // see https://github.com/kpmoran/OpenTerm/commit/022dcfaf425645f63d4721b1353c31614943bc32
+                let task = Process()
+                task.launchPath = "/bin/bash"
+                task.arguments = ["-c", "pluginkit -e use -i com.andrewringler.git-annex-mac.git-annex-finder ; killall Finder"]
+                task.launch()
+                
+                while true {
+                    let allKeys = defaults!.dictionaryRepresentation().keys
+                    for key in allKeys {
+                        if key.starts(with: "gitannex.") {
+                            if(defaults!.string(forKey: key)! == "request"){
+                                var url :String = key
+                                url.removeFirst("gitannex.".count)
+                                NSLog(".")
+                                let status = GitAnnexQueries.gitAnnexPathInfo(for: URL(fileURLWithPath: url), in: (myFolderURL as NSURL).path!)
+                                
+                                if status == "present" {
+                                    defaults!.set("present", forKey: key)
+                                } else if status == "absent" {
+                                    defaults!.set("absent", forKey: key)
+                                } else {
+                                    defaults!.set("unknown", forKey: key)
+                                }
                             }
                         }
                     }
+                    sleep(1)
                 }
-                sleep(1)
+            } else {
+                NSLog("did not find any folders to watch, quiting")
+                // stop Finder Sync extension
+                let task = Process()
+                task.launchPath = "/bin/bash"
+                task.arguments = ["-c", "pluginkit -e ignore -i com.andrewringler.git-annex-mac.git-annex-finder ; killall Finder"]
+                task.launch()
+                
+                exit(0)
             }
-        } else {
-            // stop Finder Sync extension
-            let task = Process()
-            task.launchPath = "/bin/bash"
-            task.arguments = ["-c", "pluginkit -e ignore -i com.andrewringler.git-annex-mac.git-annex-finder ; killall Finder"]
-            task.launch()
-
-            exit(0)
         }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        NSLog("quiting")
+        
+        // stop Finder Sync extension
+        let task = Process()
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", "pluginkit -e ignore -i com.andrewringler.git-annex-mac.git-annex-finder ; killall Finder"]
+        task.launch()
+    }
+    
+    @objc func printQuote(_ sender: Any?) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+        
+//        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+//        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit git-annex-turtle", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
     }
 }
 
