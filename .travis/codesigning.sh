1ed525cd090c5467567647764b3c799808742f58
Andrew Ringler <public@andrewringler.com>
2018-04-17 10:03:18 -0400

    removed certificates from code signing

--- .travis/codesigning.sh
+++ .travis/codesigning.sh
@@ -12,8 +12,8 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./Provisioning/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./Provisioning/certs/distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./Provisioning/certs/distribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+# security import ./Provisioning/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+# security import ./Provisioning/certs/distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+# security import ./Provisioning/certs/distribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
-security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
+# security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
