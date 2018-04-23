b5189e111667c49fd072a1c9908f5de93b25f29a
Andrew Ringler <public@andrewringler.com>
2018-04-23 14:09:59 -0400

    and more paths, travis release testing

--- .travis/ssh_keys.sh
+++ .travis/ssh_keys.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-openssl aes-256-cbc -K $encrypted_de59f173fd1c_key -iv $encrypted_de59f173fd1c_iv -in id_rsa_andrewringlerdownloads.enc -out ~/.ssh/id_rsa_andrewringlerdownloads -d
+openssl aes-256-cbc -K $encrypted_de59f173fd1c_key -iv $encrypted_de59f173fd1c_iv -in .travis/id_rsa_andrewringlerdownloads.enc -out ~/.ssh/id_rsa_andrewringlerdownloads -d
 
-cat known_hosts >> ~/.ssh/known_hosts
+cat .travis/known_hosts >> ~/.ssh/known_hosts
 
 chmod 700 ~/.ssh
 chmod 644 ~/.ssh/authorized_keys
