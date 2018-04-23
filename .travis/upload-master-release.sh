f326cafac6eca7a04f055d509324646422b0951c
Andrew Ringler <public@andrewringler.com>
2018-04-23 10:18:20 -0400

    cleanup upload release script

--- .travis/upload-master-release.sh
+++ .travis/upload-master-release.sh
@@ -22,19 +22,30 @@ echo "Uploading $DMG_NAME to downloads.andrewringler.com…"
 # see https://docs.travis-ci.com/user/best-practices-security/
 # travi-ci.org does not support SSH keys, use passwords instead
 # https://unix.stackexchange.com/a/187368
+# https://stackoverflow.com/a/23632210/8671834
+# Reads passwords from environment variables, set these up at
+# https://travis-ci.org/andrewringler/git-annex-turtle/settings
 
 > /dev/null 2>&1 /usr/bin/expect <<EOD
-set timeout 30
+set timeout 45
 spawn scp $DMG_PATH ${TURTLE_DEPLOY_DOWNLOADS_USER}@downloads.andrewringler.com:~/downloads.andrewringler.com/git-annex-turtle/$DMG_NAME
-expect "password:"
-send "${TURTLE_DEPLOY_DOWNLOADS_PASS}\n"
-interact
-expect eof
+
+expect {
+	"password:" { send "${TURTLE_DEPLOY_DOWNLOADS_PASS}\r"; exp_continue }
+	"Permission denied" { exit 1 }
+	timeout { puts "timeout uploading DMG"; exit 1 }
+	expect eof	
+}
+
+lassign [wait] pid spawnid os_error_flag status
+exit $status
+
 EOD
 
 if [ $? -eq 0 ]
 then
 	echo "Successfully uploaded $DMG_NAME to downloads.andrewringler.com"
+	exit
 else 
 	echo "error uploading $DMG_NAME to downloads.andrewringler.com"
 	exit -1
