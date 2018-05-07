andrewringler/git-annex-turtle

commit 98b60513a2725f3265f3920a4301cf5aad26cd93

fix: prevent most git-annex-turtle tasks from running when not on master branch, interim fix until branching is supported

src/git-annex-mac/Info.plist
@@ -23,7 +23,7 @@
 	<key>CFBundleVersion</key>
 	<string>1</string>
 	<key>GIT_COMMIT_HASH</key>
-	<string>f8c1ebc</string>
+	<string>093e41f</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>$(MACOSX_DEPLOYMENT_TARGET)</string>
 	<key>LSUIElement</key>

commit 2178e2efeb7e28022a7d064a9c0d2dacc1743115

fixed, handling of direct-mode repositories, broken in 98b60513a2

src/git-annex-mac/HandleCommandRequests.swift
@@ -43,7 +43,7 @@ class HandleCommandRequests: StoppableService {
                 if watchedFolder.uuid.uuidString == commandRequest.watchedFolderUUIDString {
                     // Is this a Git Annex Command?
                     if commandRequest.commandType.isGitAnnex {
-                        let status = gitAnnexQueries.gitAnnexCommand(for: commandRequest.pathString, in: watchedFolder.pathString, cmd: commandRequest.commandString)
+                        let status = gitAnnexQueries.gitAnnexCommand(for: commandRequest.pathString, in: watchedFolder.pathString, cmd: commandRequest.commandString, limitToMasterBranch: true)
                         if !status.success {
                             // git-annex has very nice error message, use them as-is
                             dialogs.dialogGitAnnexWarn(title: status.error.first ?? "git-annex: error", message: status.output.joined(separator: "\n"))
@@ -55,7 +55,7 @@ class HandleCommandRequests: StoppableService {
                     
                     // Is this a Git Command?
                     if commandRequest.commandType.isGit {
-                        let status = gitAnnexQueries.gitCommand(for: commandRequest.pathString, in: watchedFolder.pathString, cmd: commandRequest.commandString)
+                        let status = gitAnnexQueries.gitCommand(for: commandRequest.pathString, in: watchedFolder.pathString, cmd: commandRequest.commandString, limitToMasterBranch: true)
                         if !status.success {
                             dialogs.dialogGitAnnexWarn(title: status.error.first ?? "git: error", message: status.output.joined(separator: "\n"))
                         } else {

src/git-annex-mac/Info.plist
@@ -23,7 +23,7 @@
 	<key>CFBundleVersion</key>
 	<string>1</string>
 	<key>GIT_COMMIT_HASH</key>
-	<string>98b6051</string>
+	<string>aa2a58e</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>$(MACOSX_DEPLOYMENT_TARGET)</string>
 	<key>LSUIElement</key>

src/git-annex-macTests/TestingUtil.swift
@@ -65,6 +65,11 @@ class TestingUtil {
         return nil
     }
     
+    class func setDirectMode(for watchedFolder: WatchedFolder, gitAnnexQueries: GitAnnexQueries, file: StaticString = #file, line: UInt = #line) {
+        let gitDirectMode = gitAnnexQueries.gitAnnexCommand(in: watchedFolder.pathString, cmd: CommandString.direct, limitToMasterBranch: false)
+        if !gitDirectMode.success { XCTFail("unable to switch to direct mode \(gitDirectMode.error)", file: file, line: line)}
+    }
+    
     class func writeToFile(content: String, to fileName: String, in watchedFolder: WatchedFolder, file: StaticString = #file, line: UInt = #line) {
         let url = PathUtils.url(for: fileName, in: watchedFolder)
         do {
@@ -98,12 +103,12 @@ class TestingUtil {
     }
     
     class func gitAnnexAdd(file: String, in watchedFolder: WatchedFolder, gitAnnexQueries: GitAnnexQueries, sourcefile: StaticString = #file, sourceline: UInt = #line) {
-        let gitAddResult = gitAnnexQueries.gitAnnexCommand(for: file, in: watchedFolder.pathString, cmd: CommandString.add)
+        let gitAddResult = gitAnnexQueries.gitAnnexCommand(for: file, in: watchedFolder.pathString, cmd: CommandString.add, limitToMasterBranch: false)
         if !gitAddResult.success { XCTFail("unable to add file \(gitAddResult.error)", file: sourcefile, line: sourceline)}
     }
 
     class func gitCommit(_ commitMessage: String, in watchedFolder: WatchedFolder, gitAnnexQueries: GitAnnexQueries, file: StaticString = #file, line: UInt = #line) {
-        let result = gitAnnexQueries.gitCommit(in: watchedFolder.pathString, commitMessage: commitMessage)
+        let result = gitAnnexQueries.gitCommit(in: watchedFolder.pathString, commitMessage: commitMessage, limitToMasterBranch: false)
         if !result.success { XCTFail("unable to git commit \(result.error)", file: file, line: line)}
     }
 

src/git-annex-shared/Constants.swift
@@ -306,6 +306,7 @@ enum CommandString: String {
     case initCmd = "init"
     case numCopies = "numcopies"
     case commit = "commit"
+    case direct = "direct"
 }
 
 

src/git-annex-shared/GitAnnexQueries.swift
@@ -131,13 +131,19 @@ class GitAnnexQueries {
             }
             
             // prevent queries on anything other than the master git branch
+            // TODO this guard is not entirely safe, since the branch could change after
+            // we have called this guard, see https://git-annex.branchable.com/todo/add_a_--branch_to_applicable_git-annex_commands/ for a brief discussion on ensuring
+            // git-annex commands apply to a certain branch
             var branchGuard: String = ""
             if limitToMasterBranch {
                 guard let gitCmd = preferences.gitBin() else {
                     TurtleLog.debug("Requested limitToMasterBranch, but git command is missing")
                     return
                 }
-                branchGuard = "if [ $(\(gitCmd) symbolic-ref --short HEAD 2>/dev/null) != \"master\" ]; then exit 1; fi && "
+                
+                // https://stackoverflow.com/a/1593487/8671834
+                branchGuard = "if [[ $(git symbolic-ref --short -q HEAD 2>/dev/null | sed -e \"s/^annex\\/direct\\///\") != \"master\" ]]; then exit 1; fi && "
+                TurtleLog.debug(branchGuard)
             }
             
     //        let task = Process()
@@ -211,13 +217,13 @@ class GitAnnexQueries {
             return false
         }
         
-        let createGitRepoResult = gitCommand(in: path, cmd: CommandString.initCmd)
+        let createGitRepoResult = gitCommand(in: path, cmd: CommandString.initCmd, limitToMasterBranch: false)
         if !createGitRepoResult.success {
             TurtleLog.error("Could not create git repo in \(path)")
             return false
         }
         
-        let initGitAnnexRepo = gitAnnexCommand(in: path, cmd: CommandString.initCmd)
+        let initGitAnnexRepo = gitAnnexCommand(in: path, cmd: CommandString.initCmd, limitToMasterBranch: false)
         if !initGitAnnexRepo.success {
             TurtleLog.error("Could not init git annex repo in \(path)")
             return false
@@ -226,13 +232,13 @@ class GitAnnexQueries {
         return true
     }
     
-    func gitAnnexCommand(in workingDirectory: String, cmd: CommandString) -> (success: Bool, error: [String], output: [String], commandRun: String) {
+    func gitAnnexCommand(in workingDirectory: String, cmd: CommandString, limitToMasterBranch: Bool) -> (success: Bool, error: [String], output: [String], commandRun: String) {
         let commandRun = "git-annex " + cmd.rawValue
         guard let gitAnnexCmd = preferences.gitAnnexBin() else {
             return (false, [], [], commandRun)
         }
         
-        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitAnnexCmd, limitToMasterBranch: true, args: cmd.rawValue)
+        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitAnnexCmd, limitToMasterBranch: limitToMasterBranch, args: cmd.rawValue)
         
         if status != 0 {
             TurtleLog.error("\(commandRun) status= \(status) output=\(output) error=\(error)")
@@ -240,14 +246,14 @@ class GitAnnexQueries {
         
         return (status == 0, error, output, commandRun)
     }
-    func gitAnnexCommand(for path: String, in workingDirectory: String, cmd: CommandString) -> (success: Bool, error: [String], output: [String], commandRun: String) {
+    func gitAnnexCommand(for path: String, in workingDirectory: String, cmd: CommandString, limitToMasterBranch: Bool) -> (success: Bool, error: [String], output: [String], commandRun: String) {
         let commandRun = "git-annex " + cmd.rawValue + " \"" + path + "\""
         guard let gitAnnexCmd = preferences.gitAnnexBin() else {
             TurtleLog.debug("could not find a valid git-annex application")
             return (false, [], [], commandRun)
         }
 
-        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitAnnexCmd, limitToMasterBranch: true, args: cmd.rawValue, "\"\(path)\"")
+        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitAnnexCmd, limitToMasterBranch: limitToMasterBranch, args: cmd.rawValue, "\"\(path)\"")
         
         if status != 0 {
             TurtleLog.error("\(commandRun) status= \(status) output=\(output) error=\(error)")
@@ -255,42 +261,42 @@ class GitAnnexQueries {
         
         return (status == 0, error, output, commandRun)
     }
-    func gitCommand(for path: String, in workingDirectory: String, cmd: CommandString) -> (success: Bool, error: [String], output: [String], commandRun: String) {
+    func gitCommand(for path: String, in workingDirectory: String, cmd: CommandString, limitToMasterBranch: Bool) -> (success: Bool, error: [String], output: [String], commandRun: String) {
         let commandRun = "git " + cmd.rawValue + "\"" + path + "\""
         guard let gitCmd = preferences.gitBin() else {
             TurtleLog.debug("could not find a valid git application")
             return (false, [], [], commandRun)
         }
         
-        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitCmd, limitToMasterBranch: true, args: cmd.rawValue, "\"\(path)\"")
+        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitCmd, limitToMasterBranch: limitToMasterBranch, args: cmd.rawValue, "\"\(path)\"")
         
         if status != 0 {
             TurtleLog.error("\(commandRun) status= \(status) output=\(output) error=\(error)")
         }
         return (status == 0, error, output, commandRun)
     }
-    func gitCommand(in workingDirectory: String, cmd: CommandString) -> (success: Bool, error: [String], output: [String], commandRun: String) {
+    func gitCommand(in workingDirectory: String, cmd: CommandString, limitToMasterBranch: Bool) -> (success: Bool, error: [String], output: [String], commandRun: String) {
         let commandRun = "git " + cmd.rawValue
         guard let gitCmd = preferences.gitBin() else {
             TurtleLog.debug("could not find a valid git application")
             return (false, [], [], commandRun)
         }
 
-        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitCmd, limitToMasterBranch: true, args: cmd.rawValue)
+        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitCmd, limitToMasterBranch: limitToMasterBranch, args: cmd.rawValue)
         
         if status != 0 {
             TurtleLog.error("\(commandRun) status= \(status) output=\(output) error=\(error)")
         }
         return (status == 0, error, output, commandRun)
     }
-    func gitCommit(in workingDirectory: String, commitMessage: String) -> (success: Bool, error: [String], output: [String], commandRun: String) {
+    func gitCommit(in workingDirectory: String, commitMessage: String, limitToMasterBranch: Bool) -> (success: Bool, error: [String], output: [String], commandRun: String) {
         let commandRun = "git commit \"" + commitMessage + "\""
         guard let gitCmd = preferences.gitBin() else {
             TurtleLog.debug("could not find a valid git application")
             return (false, [], [], commandRun)
         }
         
-        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitCmd, limitToMasterBranch: true, args: CommandString.commit.rawValue, "-m", "\"" + commitMessage.escapeString() + "\"")
+        let (output, error, status) = runCommand(workingDirectory: workingDirectory, cmd: gitCmd, limitToMasterBranch: limitToMasterBranch, args: CommandString.commit.rawValue, "-m", "\"" + commitMessage.escapeString() + "\"")
         
         if status != 0 {
             TurtleLog.error("\(commandRun) status= \(status) output=\(output) error=\(error)")
@@ -328,7 +334,7 @@ class GitAnnexQueries {
     }
     
     func gitAnnexSetNumCopies(numCopies: Int, in watchedFolder: WatchedFolder) -> (success: Bool, error: [String], output: [String], commandRun: String) {
-        return gitAnnexCommand(for: String(numCopies), in: watchedFolder.pathString, cmd: .numCopies)
+        return gitAnnexCommand(for: String(numCopies), in: watchedFolder.pathString, cmd: .numCopies, limitToMasterBranch: false)
     }
     
     func gitAnnexAllFilesLackingCopies(in watchedFolder: WatchedFolder) -> Set<String>? {

src/git-annex-macTests/gitAnnexQueriesTests.swift
@@ -41,7 +41,7 @@ class gitAnnexQueriesTests: XCTestCase {
         let file1Path = "a.txt"
         TestingUtil.writeToFile(content: "some text", to: file1Path, in: watchedFolder!)
         
-        let gitAddResult = gitAnnexQueries!.gitAnnexCommand(for: file1Path, in: watchedFolder!.pathString, cmd: CommandString.add)
+        let gitAddResult = gitAnnexQueries!.gitAnnexCommand(for: file1Path, in: watchedFolder!.pathString, cmd: CommandString.add, limitToMasterBranch: false)
         if !gitAddResult.success { XCTFail("unable to add file \(gitAddResult.error)")}
         
         // Path in Root
@@ -53,7 +53,7 @@ class gitAnnexQueriesTests: XCTestCase {
         let file2Path = "ok/b.txt"
         TestingUtil.writeToFile(content: "some text again", to: file2Path, in: watchedFolder!)
 
-        let gitAddResult2 = gitAnnexQueries!.gitAnnexCommand(for: file2Path, in: watchedFolder!.pathString, cmd: CommandString.add)
+        let gitAddResult2 = gitAnnexQueries!.gitAnnexCommand(for: file2Path, in: watchedFolder!.pathString, cmd: CommandString.add, limitToMasterBranch: false)
         if !gitAddResult2.success { XCTFail("unable to add file \(gitAddResult2.error)")}
         let children2 = gitAnnexQueries!.immediateChildrenNotIgnored(relativePath: "ok", in: watchedFolder!)
         XCTAssertEqual(Set(children2), Set([file2Path]))
@@ -62,7 +62,7 @@ class gitAnnexQueriesTests: XCTestCase {
     func testGitAnnexAllFilesLackingCopiesLacking() {
         TestingUtil.gitAnnexCreateAndAdd(content: "hello", to: "file1.txt", in: watchedFolder!, gitAnnexQueries: gitAnnexQueries!)
         TestingUtil.gitAnnexCreateAndAdd(content: "hello again", to: "file2.txt", in: watchedFolder!, gitAnnexQueries: gitAnnexQueries!)
-        gitAnnexQueries!.gitAnnexCommand(for: "2", in: watchedFolder!.pathString, cmd: .numCopies)
+        gitAnnexQueries!.gitAnnexCommand(for: "2", in: watchedFolder!.pathString, cmd: .numCopies, limitToMasterBranch: false)
         let filesLackingCopies = gitAnnexQueries!.gitAnnexAllFilesLackingCopies(in: watchedFolder!)
         XCTAssertEqual(filesLackingCopies, Set(["file1.txt", "file2.txt"]))
     }

src/git-annex-macTests/watchGitAndFinderForUpdatesTests.swift
@@ -45,6 +45,7 @@ class watchGitAndFinderForUpdatesTests: XCTestCase {
         
         repo1 = TestingUtil.createInitGitAnnexRepo(at: "\(testDir!)/repo1", gitAnnexQueries: gitAnnexQueries!)
         repo2 = TestingUtil.createInitGitAnnexRepo(at: "\(testDir!)/repo2", gitAnnexQueries: gitAnnexQueries!)
+        TestingUtil.setDirectMode(for: repo2!, gitAnnexQueries: gitAnnexQueries!)
     }
     
     override func tearDown() {